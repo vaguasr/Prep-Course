fdb69263a2659f721081e7d087abf9bc82f7adcf
Victor Aguas Ramos <vaguasr@MacBook-Pro-de-Victor-2.local>
2022-11-22 06:36:20 -0500

    Finish Homework

---
+++ 02-JS-I/homework/Homework explicaciones.js
@@ -0,0 +1,21 @@
+// * Variables
+// Son un tipo de repositorio o almacen de datos que permite que le sea asignada la informacion mas de una vez. Es decir que se puede sobre escribir las veces que sea necesario.
+// Estas pueden capturar strings, numeros o booleanos.
+
+// * Strings
+// Son una cadena de datos del tipo texto, compuesta por una o mas letras o simbolos.
+// Para ser leidas como string se deben utilizar ' '. Ejemplo 'Hola'.
+// Los numeros tambien pueden ser tomados como texto, siempre y cuando esten dentro de las comillas ' '.
+
+// * Funciones
+// Es un codigo o proceso mediante el cual se utiliza unos valores de entrada llamados Argumentos para que sean procesados y obtener un resultado o salida.
+// Los Argumentos: pueden ser numeros, strings (texto) o booleanos.
+// El objetivo de la funcion en la que procesa los argumentos ya sea para realizar un analisis, comparacion u operacion
+// Ejemplos: Comparar si dos strings tienen la misma longitud, comparar dos numeros para saber cual es mayor o realizar un calculo con los numeros ingresados como argumentos.
+// * 'return' 
+// Es un comando que le indica a la funcion cual es el dato de salida, dentro de la funcion se pueden realizar muchas operaciones. Sin embargo la funcion arrojara lo que se le indique a traves del comando return 
+// La funcion solo puede arrojar como dato de salida un unico return, por tanto devolvera la informacion del primer return que considere como ejecutable.
+// La unica forma de extraer un dato que se encuentre o que sea obtenido al interior de una funcion es utlizando el comando return.
+
+// * Declaraciones
+//
